d1792a03780f7508356813e87059997114b2000a
Frankie Fong <frankiefonghk@yahoo.com>
2016-07-20 17:17:01 +0800

    Custom Field Name localization
    - Extract and localize (zh_TW) 'Sale or Wanted'
    - Fix translation error (zh_TW) of 'Outside Hong Kong' location.

--- DB_Values/dump_values_to_pot.py
+++ DB_Values/dump_values_to_pot.py
@@ -19,27 +19,28 @@ msgid_dict['pageDesc'] = OcTPreference.get(OcTPreference.s_name == 'pageDesc').s
 
 customFields = OcTMetaFields.select().where(OcTMetaFields.s_slug != 'new-custom-field');
 for cf in customFields:
-	if cf.e_type == 'RADIO' or cf.e_type == 'DROPDOWN' or cf.e_type == 'CHECKBOX':
-		cf_options = cf.s_options.split(',');
-		for opt in cf_options:
-			msgid_dict[cf.s_name + '_' + opt] = opt;
-
+    if cf.e_type == 'RADIO' or cf.e_type == 'DROPDOWN' or cf.e_type == 'CHECKBOX':
+        msgid_dict[cf.s_name + '_field'] = cf.s_name;
+        cf_options = cf.s_options.split(',');
+        for opt in cf_options:
+            msgid_dict[cf.s_name + '_' + opt] = opt;
+        
 
 regions = OcTRegion.select();
 for r in regions:
-	msgid_dict['region_' + str(r.pk_i)] = r.s_name;
+    msgid_dict['region_' + str(r.pk_i)] = r.s_name;
 
 countries = OcTCountry.select();
 for c in countries:
-	msgid_dict['country_' + c.pk_c_code] = c.s_name;
-	
+    msgid_dict['country_' + c.pk_c_code] = c.s_name;
+    
 cities = OcTCity.select();
 for c in cities:
-	msgid_dict['city_' + str(c.pk_i)] = c.s_name;
-	
+    msgid_dict['city_' + str(c.pk_i)] = c.s_name;
+    
 cityAreas = OcTCityArea.select();
 for c in cityAreas:
-	msgid_dict['cityarea_' + str(c.pk_i)] = c.s_name;
+    msgid_dict['cityarea_' + str(c.pk_i)] = c.s_name;
 
 
 database.close();
@@ -49,7 +50,7 @@ print "Database closed";
 # the makepot script actually parses the PHP input file.
 print "<?php"
 for k, v in msgid_dict.items():
-	print "_e(\"%s\");" % (v);
-print "?>"			
-	
+    print "_e(\"%s\");" % (v);
+print "?>"          
+    
 
